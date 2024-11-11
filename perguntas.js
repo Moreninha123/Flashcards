f0299f573e5b83cf874f27647e7e1e4714316dcb
Morena <164919406+Moreninha123@users.noreply.github.com>
2024-11-11 14:14:33 -0300

    bloco

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criarCartao(
+    'Programação',
+    'O que JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+
+)
+criarCartao(
+     'Geografia',
+     'Qual é a capital da frança?',
+     'A capital da frança é Paris'
+
+)
+criarCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+
+)
